9ea528f42f03c1e3a96cc36ca78bc164021b9fde
Tarang Ranpara <tarangranpara1998@gmail.com>
2020-11-03 18:37:01 +0530

    modular upto second last task

--- main_data.py
+++ main_data.py
@@ -3,6 +3,15 @@ import os
 
 random.seed(53)
 
+def write_file(file_name, content):
+    with open(file_name, 'w') as op:
+        op.writelines(content) 
+
+def filter_data(constraint, lst):
+    string_filter = lambda string : False if string == "\n" else True
+    lst = list(filter(constraint, list(filter(string_filter, lst))))
+    return lst
+
 def details_about_DAIICT():
     with open('./About-DAIICT.txt', 'r') as ad:
         lines       = ad.readlines()
@@ -12,7 +21,7 @@ def details_about_DAIICT():
         history     = structure.copy()
         environment = structure.copy()
         recognition = structure.copy()
-        accredition = structure.copy()
+        accreditation = structure.copy()
 
         i = 0
         while i < len(lines):
@@ -29,50 +38,36 @@ def details_about_DAIICT():
 
             elif lines[i] == 'Accreditation\n':
                 recognition['end'] = i-1
-                accredition['start'] = i
+                accreditation['start'] = i
 
             i += 1
 
-        with open('History.txt', 'w') as h:
-            h.writelines(lines[history['start']:history['end']+1])
+        history_r = lines[history['start']:history['end']+1]
+        write_file('History.txt', history_r)
 
-        with open('Environment.txt', 'w') as e:
-            e.writelines(lines[environment['start']:environment['end']])
+        environment_r = lines[environment['start']:environment['end']]
+        write_file('Environment.txt', environment_r)
+        
+        recognition_r = lines[recognition['start']:recognition['end']]
+        write_file('Recognition.txt', recognition_r)
 
-        with open('Recognition.txt', 'w') as r:
-            r.writelines(lines[recognition['start']:recognition['end']])
+        accreditation_r = lines[accreditation['start']:]
+        write_file('Accreditation.txt', accreditation_r)
 
-        with open('Accreditation.txt', 'w') as a:
-            a.writelines(lines[accredition['start']:])
+        return {
+            'history'      :     history_r, 
+            'environment'  : environment_r, 
+            'recognition'  : recognition_r, 
+            'accreditation':accreditation_r
+        }
 
-def emails(student_name_list, random_email_files):
 
-    string_filter = lambda string : False if string == "\n" else True
+def emails(student_name_list, random_email_files, content):
 
-    recognition = []
-    with open('Recognition.txt', 'r') as rec:
-        recognition = list(filter(string_filter, rec.readlines()))
-
-    environment = []
-    with open('Environment.txt', 'r') as env:
-        environment = list(filter(string_filter, env.readlines()))
-
-    accreditation = []
-    with open('Accreditation.txt', 'r') as acc:
-        accreditation = list(filter(string_filter, acc.readlines()))
-
-    history = []
-    with open('History.txt', 'r') as hist:
-        history = list(filter(string_filter, hist.readlines()))
-
-    history = list(filter(
-        lambda string : True if (
-            string.startswith('The first wave') or 
-            string.startswith('The second wave') or 
-            string.startswith('The third wave') or
-            string.startswith('It was in the fourth wave')
-        ) else False, history))
-
+    history = content['history']
+    environment = content['environment']
+    recognition = content['recognition']
+    accreditation = content['accreditation']
 
     for random_email_file in random_email_files:
         with open(random_email_file, 'w') as remail:
@@ -93,7 +88,7 @@ def emails(student_name_list, random_email_files):
             )
 
 
-def emails_original():
+def emails_original(content):
     student_name_list = []
     with open('student_names_list.txt', 'r') as s:
         student_name_list = s.readlines()
@@ -107,11 +102,23 @@ def emails_original():
     random_email_files = [
         f'email-{i}.txt' for i in random.sample(range(1, 21), 15)
     ]
+    
+    ready_to_go = lambda string: True
+    content['environment']   = filter_data(ready_to_go, content['environment'])
+    content['recognition']   = filter_data(ready_to_go, content['recognition'])
+    content['accreditation'] = filter_data(ready_to_go, content['accreditation'])
 
-    emails(student_name_list, random_email_files)
+    history_constraint = lambda string : True if (
+            string.startswith('The first wave') or 
+            string.startswith('The second wave') or 
+            string.startswith('The third wave') or
+            string.startswith('It was in the fourth wave')) else False
+
+    content['history'] = filter_data(history_constraint, content['history'])
+    emails(student_name_list, random_email_files, content)
 
 
-def emails_with_modifications():
+def emails_with_modifications(content):
     files = list(filter(lambda string: True if string.startswith('email') else False, os.listdir())) 
     files = set(i for i in range(1,21)).difference(set(int(email.split('-')[1].split('.')[0]) for email in files))
 
@@ -119,6 +126,21 @@ def emails_with_modifications():
     
     student_name_list = ['name1@gmail.com', 'A_X_y@yahoo.co.in', 'nm123@rediff.com', 'nam_4_e@160.com']
 
-    emails(student_name_list, email_file_list)
+    ready_to_go = lambda string: True
+    content['environment']   = filter_data(ready_to_go, content['environment'])
+    content['recognition']   = filter_data(ready_to_go, content['recognition'])
+    content['accreditation'] = filter_data(ready_to_go, content['accreditation'])
 
-emails_with_modifications()
+    history_constraint = lambda string : True if (
+            string.startswith('The first wave') or 
+            string.startswith('The second wave') or 
+            string.startswith('The third wave') or
+            string.startswith('It was in the fourth wave')) else False
+
+    content['history'] = filter_data(history_constraint, content['history'])
+
+    emails(student_name_list, email_file_list, content)
+
+content = details_about_DAIICT()
+emails_original(content)
+emails_with_modifications(content)
